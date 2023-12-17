63fdf1e77f57e40edd82b21b99d726d5ef06c554
Bui Quoc Huy <wuochuy02@gmail.com>
2023-12-17 07:56:04 +0700

    first commit

--- src/utils/handlers/imageUploadClound.ts
+++ src/utils/handlers/imageUploadClound.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { NotificationToast } from "./NotificationToast";
 
-const path = "https://api.cloudinary.com/v1_1/ddtagvynp/upload";
+const path = "https://api.cloudinary.com/v1_1/dhcijuvn8/upload";
 
 export const imageUpload = async (image: any) => {
   try {
     const data = new FormData();
     data.append("file", image);
-    data.append("upload_preset", "webfood");
-    data.append("clound_name", "ddtagvynp");
+    data.append("upload_preset", "cf_prj");
+    data.append("clound_name", "dhcijuvn8");
     const res = await axios.post(path, data);
     return res.data.url;
   } catch (error: any) {
